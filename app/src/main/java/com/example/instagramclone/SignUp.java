d083f9efbe8613398ffe6413d575be5c77faa3bb
Your Name <you@example.com>
2019-07-29 11:40:18 +0530

    SignUp & Login Functionality

--- app/src/main/java/com/example/instagramclone/SignUp.java
+++ app/src/main/java/com/example/instagramclone/SignUp.java
@@ -1,6 +1,8 @@
 package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -87,7 +89,8 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
         btnSwitchActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(SignUp.this, SignUpLoginActivity.class);
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/example/instagramclone/SignUpLoginActivity.java
+++ app/src/main/java/com/example/instagramclone/SignUpLoginActivity.java
@@ -1,4 +1,74 @@
 package com.example.instagramclone;
 
-public class SignUpLoginActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import es.dmoral.toasty.Toasty;
+
+public class SignUpLoginActivity extends AppCompatActivity {
+
+    private EditText editUserName, editPassword, editUserNameLogin, editPasswordLogin;
+    private Button btnSignUpUser, btnLoginUser;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_login_activity);
+
+        editUserName = findViewById(R.id.editUserName);
+        editPassword = findViewById(R.id.editPassword);
+        btnSignUpUser = findViewById(R.id.btnSignUpUser);
+
+        btnLoginUser = findViewById(R.id.btnLoginUser);
+        editUserNameLogin = findViewById(R.id.editUserNameLogin);
+        editPasswordLogin = findViewById(R.id.editPasswordLogin);
+
+        btnSignUpUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ParseUser appUser = new ParseUser();
+                appUser.setUsername(editUserName.getText().toString());
+                appUser.setPassword(editPassword.getText().toString());
+
+                appUser.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Toasty.success(SignUpLoginActivity.this, appUser.get("username") + " is Signed Up Successfully", Toast.LENGTH_SHORT).show();
+                        } else
+                            Toasty.error(SignUpLoginActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        btnLoginUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logInInBackground(editUserNameLogin.getText().toString(),
+                        editPasswordLogin.getText().toString(), new LogInCallback() {
+                            @Override
+                            public void done(ParseUser user, ParseException e) {
+                                if (user != null && e == null) {
+                                    Toasty.success(SignUpLoginActivity.this, user.get("username") + " is Logged In Successfully", Toast.LENGTH_SHORT).show();
+                                } else
+                                    Toasty.error(SignUpLoginActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+            }
+        });
+
+    }
 }
