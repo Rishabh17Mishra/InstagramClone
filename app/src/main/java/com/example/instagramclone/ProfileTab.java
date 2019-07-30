ecb1568012596e8c60078c863c9abddaa7895d5a
Your Name <you@example.com>
2019-07-30 13:30:32 +0530

    Share & Upload Pics to Server

--- app/src/main/java/com/example/instagramclone/ProfileTab.java
+++ app/src/main/java/com/example/instagramclone/ProfileTab.java
@@ -2,12 +2,19 @@ package com.example.instagramclone;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.fragment.app.Fragment;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import es.dmoral.toasty.Toasty;
 
 
 /**
@@ -15,6 +22,8 @@ import android.view.ViewGroup;
  */
 public class ProfileTab extends Fragment {
 
+    private EditText editProfileName, editProfileBio, editProfileProfession, editProfileHobbies, editProfileSport;
+    private Button btnUpdateInfo;
 
     public ProfileTab() {
         // Required empty public constructor
@@ -25,7 +34,55 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        editProfileName = view.findViewById( R.id.editProfileName );
+        editProfileBio = view.findViewById( R.id.editProfileBio );
+        editProfileProfession = view.findViewById( R.id.editProfileProfession );
+        editProfileSport = view.findViewById( R.id.editProfileSport );
+        editProfileHobbies = view.findViewById( R.id.editProfileHobbies );
+        btnUpdateInfo = view.findViewById( R.id.btnUpdateInfo );
+
+        final ParseUser parseUser = ParseUser.getCurrentUser();
+
+        if (parseUser.get( "ProfileName" ) == null) {
+            editProfileName.setText("");
+        }else { editProfileName.setText( parseUser.get( "ProfileName" ).toString());}
+        if (parseUser.get( "ProfileBio" ) == null) {
+            editProfileBio.setText("");
+        }else { editProfileBio.setText( parseUser.get( "ProfileBio" ).toString());}
+        if (parseUser.get( "ProfileProfession" ) == null) {
+            editProfileProfession.setText("");
+        }else { editProfileProfession.setText( parseUser.get( "ProfileProfession" ).toString());}
+        if (parseUser.get( "ProfileSport" ) == null) {
+            editProfileSport.setText("");
+        }else { editProfileSport.setText( parseUser.get( "ProfileSport" ).toString());}
+        if (parseUser.get( "ProfileHobbies" ) == null) {
+            editProfileHobbies.setText("");
+        }else { editProfileHobbies.setText( parseUser.get( "ProfileHobbies" ).toString());}
+
+
+        btnUpdateInfo.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                parseUser.put( "ProfileName", editProfileName.getText().toString() );
+                parseUser.put( "ProfileBio", editProfileBio.getText().toString() );
+                parseUser.put( "ProfileProfession", editProfileProfession.getText().toString() );
+                parseUser.put( "ProfileSport", editProfileSport.getText().toString());
+                parseUser.put( "ProfileHobbies", editProfileHobbies.getText().toString());
+
+                parseUser.saveInBackground( new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) { Toasty.success( getContext(),"Info Updated", Toasty.LENGTH_SHORT ).show(); }
+                        else { Toasty.error( getContext(),e.getMessage(), Toasty.LENGTH_SHORT ).show();}
+                    }
+                } );
+            }
+        } );
+
+        return view;
+
     }
 
 }
--- app/src/main/java/com/example/instagramclone/SharePicsTab.java
+++ app/src/main/java/com/example/instagramclone/SharePicsTab.java
@@ -1,31 +1,150 @@
 package com.example.instagramclone;
 
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
+
+import es.dmoral.toasty.Toasty;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SharePicsTab extends Fragment {
+public class SharePicsTab extends Fragment implements View.OnClickListener{
 
+    private ImageView imageView;
+    private EditText textImgName;
+    private Button btnShareImage;
+    Bitmap receivedImageBitmap;
 
     public SharePicsTab() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_share_pics_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_share_pics_tab, container, false);
+
+        imageView = view.findViewById(R.id.imageView);
+        textImgName = view.findViewById( R.id.textImgName );
+        btnShareImage = view.findViewById( R.id.btnShareImage );
+
+        imageView.setOnClickListener(SharePicsTab.this);
+        btnShareImage.setOnClickListener(SharePicsTab.this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.imageView :
+                //Below Code checks for permission from user
+                if (Build.VERSION.SDK_INT >= 23 && ActivityCompat.checkSelfPermission(getContext(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                {
+                    requestPermissions(new String[]{
+                            Manifest.permission.READ_EXTERNAL_STORAGE},1000);
+                }else {
+                    getChosenImage();
+                }
+                break;
+            case R.id.btnShareImage :
+                if (receivedImageBitmap != null) {
+                    if (textImgName.getText().toString().equals( "" )) {
+                        Toasty.error( getContext(),"Error: You must specify a Description", Toasty.LENGTH_SHORT ).show();
+                    }else {
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                        receivedImageBitmap.compress(Bitmap.CompressFormat.PNG,100, byteArrayOutputStream);
+                        byte[] bytes = byteArrayOutputStream.toByteArray();
+                        ParseFile parseFile = new ParseFile( "pic.png",bytes );
+                        ParseObject parseObject = new ParseObject( "Photo" );
+                        parseObject.put( "pic", parseFile );
+                        parseObject.put( "image_description", textImgName.getText().toString() );
+                        parseObject.put( "username", ParseUser.getCurrentUser().getUsername() );
+                        final ProgressDialog dialog = new ProgressDialog( getContext() );
+                        dialog.setMessage( "Loading..." );
+                        dialog.show();
+
+                        parseObject.saveInBackground( new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                if (e == null) {
+                                    Toasty.success( getContext(),"Successfuly Uploaded!!!", Toasty.LENGTH_SHORT ).show();
+                                } else { Toasty.error( getContext(),"Unknown Error: " + e.getMessage(), Toasty.LENGTH_SHORT ).show(); }
+                                dialog.dismiss();
+                            }
+                        } );
+                    }
+                }else { Toasty.error( getContext(),"Error: You must select a pic", Toasty.LENGTH_SHORT ).show(); }
+                break;
+        }
+    }
+
+    private void getChosenImage() {
+        Toasty.success(getContext(),"Now we can access the images", Toasty.LENGTH_SHORT).show();
+
+        Intent intent = new Intent( Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI );
+        startActivityForResult( intent, 2000 );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult( requestCode, permissions, grantResults );
+        if (requestCode == 1000) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) { getChosenImage(); }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult( requestCode, resultCode, data );
+        if (requestCode == 2000) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                    Cursor cursor = getActivity().getContentResolver().query( selectedImage, filePathColumn,null,null,null );
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex( filePathColumn[0] );
+                    String picPath = cursor.getString( columnIndex );
+                    cursor.close();
+                    receivedImageBitmap = BitmapFactory.decodeFile( picPath );
+                    imageView.setImageBitmap( receivedImageBitmap );
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
